e283361211ccf591cb594cbc5f8ccd98abbcd5b0
Nikita Shishov <panassonik2013@yandex.ru>
2020-03-14 19:40:35 +0300

    fix: fixed redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return redirect(env('FRONTEND_URL'));
+            return redirect('http://localhost:5000');
         }
     }
 }
