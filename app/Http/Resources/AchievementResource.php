260385089b4a21d40c44e9f1f459d53ea282ab4b
Nikita Shishov <panassonik2013@yandex.ru>
2020-03-16 23:28:35 +0300

    feat: added avhievement icons diff

--- app/Http/Resources/AchievementResource.php
+++ app/Http/Resources/AchievementResource.php
@@ -19,7 +19,7 @@ class AchievementResource extends JsonResource
             'id' => $this->id,
             'label' => $this->label,
             'description' => $this->description,
-            'icon_url' => $this->icon_url,
+            'icon_url' => $this->when(auth()->user()->achievements->contains(Achievement::find($this->id)), "$this->icon_url.png", $this->icon_url."_bw.png"),
             'completed' => $this->when(auth()->user()->achievements->contains(Achievement::find($this->id)), true, false),
         ];
     }
--- database/seeds/AchievementTableSeeder.php
+++ database/seeds/AchievementTableSeeder.php
@@ -14,37 +14,37 @@ class AchievementTableSeeder extends Seeder
         \App\Models\Achievement::create([
             'label' => 'Первопроходец',
             'description' => 'Выполнил свое первое задание',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'pervoprohodec',
         ]);
         \App\Models\Achievement::create([
             'label' => 'Я на что-то способен',
             'description' => 'Выполнил 10 заданий',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'sposoben',
         ]);
         \App\Models\Achievement::create([
             'label' => 'Киберспортивная единица',
             'description' => 'Выполнил 20 заданий',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'cybersport',
         ]);
         \App\Models\Achievement::create([
             'label' => 'Коронован',
             'description' => 'Выполнил 30 заданий',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'koronovan',
         ]);
         \App\Models\Achievement::create([
             'label' => 'Самые быстрые пальцы на Диком Западе',
             'description' => 'Поучавствовал в задании с кликером',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'faster',
         ]);
         \App\Models\Achievement::create([
             'label' => 'Кладоискатель',
             'description' => 'Выполнил все задания в AR',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'kladoiskatel',
         ]);
         \App\Models\Achievement::create([
             'label' => 'Показал прибыль',
             'description' => 'Выбил 100 флекскоинов в Колесе фортуны',
-            'icon_url' => env('FRONTEND_URL'),
+            'icon_url' => 'pribil',
         ]);
     }
 }
